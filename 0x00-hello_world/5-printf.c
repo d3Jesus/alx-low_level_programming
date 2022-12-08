e98b554ebf7d2e95814c639c104521f6bf5bc7b4
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-07 22:44:48 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	printf("%c", "with proper grammar, but the outcome is a pieace of art,\n");
+	printf("with proper grammar, but the outcome is a pieace of art,\n");
 	return (0);
 }
