da999dbacf31b1766ab34d727e508d0b9855caf3
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-12 00:34:18 -0800

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description - Prints all possible combinations of two two-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int firstDigit;
+	int secondDigit;
+
+	for (firstDigit = 0; firstDigit < 9; firstDigit++)
+		for (secondDigit = 1; secondDigit < 10; secondDigit++)
+		{
+			putchar((firstDigit % 10) + '0');
+			putchar((secondDigit % 10) + '0');
+
+			if (firstDigit == 8 && secondDigit == 9)
+				break;
+
+			putchar(',');
+			putchar(' ');
+		}
+	putchar(*"\n");
+	return (0);
+}
