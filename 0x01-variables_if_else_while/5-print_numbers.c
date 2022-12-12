fec86673eb4c4a330169dbec5f2e49941e64bf1c
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-11 22:34:12 -0800

    Fixing betty coding style

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Description - Prints all single digit numbers of base 10 starting from 0 followed
- * by a new line
+ * Description - Prints all single digit numbers of base 10
+ * starting from 0 followed by a new line
  *
  * Return: Always 0 (Success)
  */
