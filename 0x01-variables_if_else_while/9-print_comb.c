a78830fefa4a5f1602758d806c58b66e6a538dcd
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-12 00:08:47 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Prints all single digit numbers of base 10
+ * starting from '0', separeted by ',' followed by a space.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		if (digit > 0)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar(digit + '0');
+	}
+
+	putchar(*"\n");
+	return (0);
+}
