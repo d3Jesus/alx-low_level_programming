e7c779e31af1dd39087444258d1c559a60a476c4
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-11 21:39:22 -0800

    Fix betty trailing whitespace and added description fom main function

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,23 +2,26 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
- * main - Determine if a number is positive, negative or zero
+/**
+ * main - Entry point
+ *
+ * Description - Determine if a number is positive, negative or zero
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX /2;
+
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
-		printf("%d is zero\n", n);	
+		printf("%d is zero\n", n);
+
 	return (0);
 }
