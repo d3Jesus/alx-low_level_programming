e321c8bc2bd1011b770f0a81d9d6584fc83b3e1b
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-13 12:10:16 -0800

    Fix task 11: correct output

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@
 void print_to_98(int n)
 {
 	if (n == 98)
-		printf("%d\n", n);
+		printf("%d", n);
 	else if (n < 98)
 		do
 		{
@@ -27,6 +27,10 @@ void print_to_98(int n)
 		do
 		{
 			printf("%d, ", n);
+			if (n > 98)
+				printf(", ");
 			n--;
 		} while (n >= 98);
+
+	printf("\n");
 }
