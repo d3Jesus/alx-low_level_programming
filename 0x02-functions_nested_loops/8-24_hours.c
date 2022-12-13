5245d1820ca34d129ec78c1de610075f2553bb35
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-13 11:11:23 -0800

    I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function that prints every minute of the day of
+ * Jack Bauer, starting from 00:00 to 23:59
+ *
+ * Return: Nothing
+ */
+
+void jack_bauer(void)
+{
+	int minute;
+	int hour;
+	
+	for (hour = 0; hour <= 23; hour++)
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+
+			_putchar(':');
+
+			_putvhar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+}
