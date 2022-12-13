f703855a3fb7e417e955064c5558bd4d4ebb9e16
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-13 04:51:41 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints the sign of a number
+ *
+ * @c: the number to verify
+ *
+ * Return: 
+ * 1 if n is greater than zero
+ * 0 if n is zero
+ * -1 if n is less than zero
+ */
+
+int print_sign(int n)
+{
+	int result;
+
+	if (n > 0)
+	{
+		result = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		result = 0;
+		_putchar('0');
+	}
+	else
+	{
+		result = -1;
+		_putchar('-');
+	}
+
+	return (result);
+}
