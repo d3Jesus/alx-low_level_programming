c2c30d254aac824d72bd18c3d8fc6f9ee14dae87
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-13 01:44:39 -0800

    Fix compilation error

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,5 +10,17 @@
 
 int main(void)
 {
-	_putchar(*"_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	_putchar(*"_putchar");
+
+	return (0);
 }
