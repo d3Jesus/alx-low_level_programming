4ab9d784702fce630e753800d660c4d9e9844461
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-13 03:48:14 -0800

    Fix task 3 erros

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,12 @@
 
 /**
  * _islower - function that checks for lowercase character
- * 
+ *
  * @c: the character to check
  *
  * Return: 1 if c is lowercase
  * Otherwise 0
  */
-
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
