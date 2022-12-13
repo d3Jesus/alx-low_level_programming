8ffd8d9a5e28ee319efb50ade11bc753b84a2b8a
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-13 03:10:41 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - function that checks for lowercase character
+ *
+ * Return: 1 if c is lowercase
+ * Return: 0 if c is uppercase
+ */
+
+int _islower(int c)
+{
+	return (islower(c));
+}
