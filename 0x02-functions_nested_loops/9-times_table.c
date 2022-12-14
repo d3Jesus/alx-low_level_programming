b3cb49c5d13f69c357b79a8e34320c201d46fa52
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-13 22:53:14 -0800

    Fix task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ void times_table(void)
 
 			if (result == 0)
 				_putchar('0' + result);
-			if (result > 0 && result < 10)
+			else if (result > 0 && result < 10)
 			{
 				_putchar(' ');
 				_putchar(' ');
