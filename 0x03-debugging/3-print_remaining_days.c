7da5bbfe43369f905d5132ba22227c50f2366baf
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-15 17:24:46 +0200

    Fix task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,8 +3,6 @@
 
 /**
  *
- * main - Entry point
- *
  * print_remaining_days - takes a date and prints how many days are
  * left in the year, taking leap years into account
  * @month: month in number format
