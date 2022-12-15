670a52839bf612fc2eee83a94cdb4abff35a52ed
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-15 15:36:24 +0200

    Fix task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,6 +4,8 @@
  * main - test is the function positive_or_negative gives the
  * correct output when given a case of 0.
  *
+ * positive_or_negative - function to test
+ *
  * Return: 0
  */
 
@@ -13,5 +15,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
