8361fdd5f0c53396423efde11727e078f1d8fe5a
Yuran Cumbane <yuran.dejesus@gmail.com>
2022-12-15 05:00:14 -0800

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test is the function positive_or_negative gives the
+ * correct output when given a case of 0.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	positive_or_negative(0);
+
+	return (0);
+}
