873821a89de534e2b24a4aebd5c56075bfc9b7b1
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-01-05 07:43:22 +0200

    Fix task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,10 +15,10 @@ void more_numbers(void)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			if (i <= 9)
-				_putchar(i % 10 + '0');
-			else
+			if (i > 9)
 				_putchar(i / 10 + '0');
+			else
+				_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
