e794f3ddebc31a40f972b956a181c59a84838054
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-01-14 04:51:12 +0200

    Fix task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,10 +11,15 @@ void print_line(int n)
 {
 	int line = 0;
     
-	while (line < n)
-	{
-		_putchar('_');
-		line++;
-	}
-	_putchar('\n');
+	if (n <= 0)
+        _putchar('\n');
+    else
+    {
+        while (line < n)
+        {
+            _putchar('_');
+            line++;
+        }
+        _putchar('\n');
+    }
 }
