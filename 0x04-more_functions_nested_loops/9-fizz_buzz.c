b2ac8427cee3e657f74be5fb4969f5a6a0e98284
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-01-14 05:07:49 +0200

    Fix: Fizz-Buzz output

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,26 @@
-
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - take the fizz-buzz challenge
+ * main - Fizz-Buzz test
  *
  * Return: Always 0
  */
 
 int main(void)
 {
-	int n;
+	int i;
 
-	for (n = 1; n < 101; n++)
-	{
-		if (n % 5 == 0 && n % 3 == 0)
-			printf("FizzBuzz ");
-		else if (n % 5 == 0)
-			if (n == 100)
-				printf("Buzz");
-			else
-				printf("Buzz ");
-		else if (n % 3 == 0)
-			printf("Fizz ");
-		else
-			printf("%d ", n);
-	}
-	printf("\n");
+    for(i = 1; i <= 100; ++i)
+    {
+        if (i % 3 == 0)
+            printf("Fizz");
+        if (i % 5 == 0)
+            printf("Buzz");
+        if ((i % 3 != 0) && (i % 5 != 0))
+            printf("FizzBuzz");
+        printf("\n");
+    }
 
-	return (0);
+    return (0);
 }
